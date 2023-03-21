2538b89b67519aaa757258b8f445929258a3424c
Greed2003 <igorolivio123@gmail.com>
2023-03-21 18:00:49 -0300

    Aula2

--- Aula2 C206-L4/src/Main.java
+++ Aula2 C206-L4/src/Main.java
@@ -3,7 +3,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner
+        Scanner entrada = new Scanner(System.in); // Variavel para entrada de dados
+        float peso; //Variavel para receber o peso
+        float altura; //Variavel para receber a altura
+        float imc; // Variavel para receber o calculo
 
-    }
+        System.out.println("Entre com o seu peso: ");
+        peso = entrada.nextFloat();
+
+        System.out.println("Entre com a sua altura: ");
+        altura = entrada.nextFloat();
+
+        imc = peso / (altura * altura);
+
+        System.out.println("IMC: " + imc);
+
+        if(imc<=18.5){
+            System.out.println("Abaixo do peso!");
+        }
+
+        else if(imc<24.9){
+            System.out.println("Peso normal!");
+        }
+
+        else if(imc<29.9){
+            System.out.println("Sobrepeso!");
+        }
+
+        else if(imc<34.9){
+            System.out.println("Obesidade grau 1!");
+        }
+
+        else if(imc<39.9){
+            System.out.println("Obesidade grau 2!");
+        }
+
+        else{
+            System.out.println("Obesidade grau 3!");
+        }
+
+        entrada.close();
+}
 }
